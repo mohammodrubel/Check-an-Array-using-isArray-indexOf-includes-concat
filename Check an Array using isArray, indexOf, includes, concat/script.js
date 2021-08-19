5f6799e4d99dfbcd7f141534d813c81463459232
Mohammodrubel <74967048+mohammodrubel@users.noreply.github.com>
2021-08-19 16:56:58 +0600

    Add files via upload

---
+++ Check an Array using isArray, indexOf, includes, concat/script.js
@@ -0,0 +1,31 @@
+const friends=['akash','nafis','emrul','hasibHasan','rubel','fardinTazbeed','hashem','kashem','mohammodruebl']
+
+    function megaFriends(friends){
+        if(Array.isArray(friends)== false){
+            return 'Please enter a array Or string ';
+        }
+        let singlefriend = friends[0]
+            for (var friend of friends){
+                if (friend.length > singlefriend.length){
+                    singlefriend = friend
+                }
+            }
+        return singlefriend
+    }
+    let myBigFriend = megaFriends(friends)
+    // console.log(myBigFriend)
+        // error throw 
+            // find name 
+            // check Array
+
+            // if (friends.indexOf('NASRIN')!= -1){
+            //     console.log('EXIST YOUR VALUE')
+            // }else{
+            //     console.log('Not EXIST YOUR VALUE')
+            // }
+    // same eror throw 
+    if (friends.includes('noman')){
+        console.log('your value exist')
+    }else{
+        console.log('your value not exist')
+    }
